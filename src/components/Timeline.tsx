7fb2ee0ea065fb0c825be7d79314ed5fbf0b8351
Aaron Marks <aaron.marks@cdpehs.com>
2024-01-17 19:46:39 -0500

    Tweaking UI

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,7 +2,7 @@ import './styles/Timeline.css';
 
 const Timeline = () => {
     return (
-        <div className="timeline section justify-center">
+        <div className="timeline section">
             <div className="section-body">
                 <h1 className="overlay-header full-width section-header">Timeline</h1>
                 <div className="section-content">Timeline content of some sort.</div>
