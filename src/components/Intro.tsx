3c243b72d85de78e23a89a9e400422c634afa128
Aaron Marks <me@aaronmarks.dev>
2024-01-19 14:47:57 -0500

    New intro pic.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,12 @@
-import officePic from '../assets/office.jpg';
+// import officePic from '../assets/office.jpg';
+import winterIntro from '../assets/WinterIntro.jpeg';
 import './styles/Intro.css';
 
 const Intro = () => {
   return (
     <>
         <div id="intro" className="intro section">
-            <img src={officePic} className="photo" />
+            <img src={winterIntro} className="photo" />
         </div>
         <div className="overlay-section absolute">
             <h1 className="overlay-header">Aaron Marks</h1>
